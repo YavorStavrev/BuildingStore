4707f09293932e3077de09d7d8330bdbbb9acccb
Plamen Draganov <plamine95@gmail.com>
2025-12-28 13:13:17 +0200

    Add Cart and CartItem JPA repositories
    Introduced CartRepository and CartItemRepository interfaces to enable CRUD operations for Cart and CartItem entities using Spring Data JPA. CartRepository includes methods to find carts by user and user ID.

---
+++ src/main/java/com/tu/javabuildingstore/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.tu.javabuildingstore.repository;
+
+import com.tu.javabuildingstore.model.Cart;
+import com.tu.javabuildingstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUser(User user);
+
+    Optional<Cart> findByUserId(Long userId);
+}
