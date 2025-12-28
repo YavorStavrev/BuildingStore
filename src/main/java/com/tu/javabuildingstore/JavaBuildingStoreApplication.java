41c3f917744bac86a99df294195397aee85cc96f
Plamen Draganov <159624750+PlamenDraganov16@users.noreply.github.com>
2025-12-28 11:13:45 +0200

    Initialized the project

---
+++ src/main/java/com/tu/javabuildingstore/JavaBuildingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tu.javabuildingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBuildingStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBuildingStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tu/javabuildingstore/JavaBuildingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.javabuildingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBuildingStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
